f5073052f3408ea0ccbc0d7efc66adbcaf2ad904
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-14 17:54:43 +0100

    adds PlayerControllerTest and modifies pom

---
+++ 01 Hello World Java/src/test/java/com/github/spring/web/PlayerControllerTest.java
@@ -0,0 +1,26 @@
+package com.github.spring.web;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+public class PlayerControllerTest {
+    private final PlayerController controller = new PlayerController();
+    private final MockMvc mvc = standaloneSetup(controller).build();
+
+    @Test
+    public void controllerTest() throws Exception {
+        MvcResult result = mvc.perform(get("/display"))
+                .andExpect(status().isOk()).andReturn();
+        SoftAssertions softly = new SoftAssertions();
+        assertThat(result.getModelAndView().getViewName()).isEqualTo("playerslist");
+        assertThat(result.getModelAndView().getModel()).containsKey("players");
+        softly.assertAll();
+    }
+}
