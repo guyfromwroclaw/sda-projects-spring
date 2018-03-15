ad97f17d766e92ac7472c613060e3d39a5ee2cb0
Bartosz Rachwalik <rachwalikbartosz@gmail.com>
2018-03-15 17:11:20 +0100

    adds PlayerServicetest

---
+++ 01 Hello World Java/src/test/java/com/github/spring/web/PlayerServiceTest.java
@@ -0,0 +1,36 @@
+package com.github.spring.web;
+
+import org.junit.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+public class PlayerServiceTest {
+
+    private final PlayerService service = new PlayerService();
+
+    @Test
+    public void addThrowExceptionForNull() {
+        assertThatThrownBy(() -> service.add(null)).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void removeThrowExceptionForNull() {
+        assertThatThrownBy(() -> service.remove(null)).isInstanceOf(NullPointerException.class);
+    }
+
+    @Test
+    public void addIncreasesSizeOfPlayersList() {
+        service.add(new Player("Robert", "Lewy"));
+        assertThat(service.findAll()).hasSize(1).containsExactly(new Player("Robert", "Lewy"));
+    }
+
+    @Test
+    public void removeDecreasesSizeOfPlayersList() {
+        service.add(new Player("Robert", "Lewy"));
+        service.add(new Player("Kuba", "Błaszczu"));
+        service.remove(new Player("Robert", "Lewy"));
+
+        assertThat(service.findAll()).hasSize(1).containsExactly(new Player("Kuba", "Błaszczu"));
+    }
+}
